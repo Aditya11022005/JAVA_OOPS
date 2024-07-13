53333728983abbdde17607203e4801d8565953b7
Aditya <pandeaditya120@gmail.com>
2024-07-13 21:16:51 +0530

    java oops concept

---
+++ src/class_obj.java
@@ -0,0 +1,19 @@
+
+class clg{
+
+    String name;
+    int roll_no;
+}
+
+public class class_obj {
+    public static void main(String[] args){
+
+        clg stud1 = new clg();
+        stud1.name = "Aditya";
+        stud1.roll_no = 60;
+
+        System.out.println("Name : " + stud1.name);
+        System.out.println("Roll_Number : " + stud1.roll_no);
+
+    }
+}
